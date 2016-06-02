3d0b3467ff4235a96419e51416ef4c5761124429
Peter Gribanov <wcode404@gmail.com>
2016-06-02 17:23:01 +0300

    change license to MIT

--- src/Service/Builder.php
+++ src/Service/Builder.php
@@ -5,7 +5,7 @@
  * @package   AnimeDb
  * @author    Peter Gribanov <info@peter-gribanov.ru>
  * @copyright Copyright (c) 2011, Peter Gribanov
- * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ * @license   http://opensource.org/licenses/MIT
  */
 
 namespace AnimeDb\Bundle\PaginationBundle\Service;
