8c64fa8f8a20d7b4d19d7399c7bbf0ae6900685f
Peter Gribanov <wcode404@gmail.com>
2016-06-03 12:50:17 +0300

    calculate offsets from methods

--- src/Service/NavigateRange.php
+++ src/Service/NavigateRange.php
@@ -55,28 +55,74 @@ class NavigateRange
     protected function buildOffset()
     {
         if ($this->left_offset < 0) {
-            // definition of offset to the left and to the right of the selected page
-            $this->left_offset = (int) floor(($this->config->getMaxNavigate() - 1) / 2);
-            $this->right_offset = (int) ceil(($this->config->getMaxNavigate() - 1) / 2);
+            $this
+                ->definitionOffset()
+                ->adjustmentLargeLeftOffset()
+                ->adjustmentLargeRightOffset()
+                ->adjustmentLowerLeftOffset();
+        }
 
-            // adjustment, if the offset is too large left
-            if ($this->config->getCurrentPage() - $this->left_offset < 1) {
-                $offset = (int) abs($this->config->getCurrentPage() - 1 - $this->left_offset);
-                $this->left_offset = $this->left_offset - $offset;
-                $this->right_offset = $this->right_offset + $offset;
-            }
+        return $this;
+    }
 
-            // adjustment, if the offset is too large right
-            if ($this->config->getCurrentPage() + $this->right_offset > $this->config->getTotalPages()) {
-                $offset = (int) abs($this->config->getTotalPages() - $this->config->getCurrentPage() - $this->right_offset);
-                $this->left_offset = $this->left_offset + $offset;
-                $this->right_offset = $this->right_offset - $offset;
-            }
+    /**
+     * Definition of offset to the left and to the right of the selected page.
+     *
+     * @return NavigateRange
+     */
+    protected function definitionOffset()
+    {
+        $this->left_offset = (int) floor(($this->config->getMaxNavigate() - 1) / 2);
+        $this->right_offset = (int) ceil(($this->config->getMaxNavigate() - 1) / 2);
 
-            // left offset should point not lower of the first page
-            if ($this->left_offset >= $this->config->getCurrentPage()) {
-                $this->left_offset = $this->config->getCurrentPage() - 1;
-            }
+        return $this;
+    }
+
+    /**
+     * Adjustment, if the offset is too large left.
+     *
+     * @return NavigateRange
+     */
+    protected function adjustmentLargeLeftOffset()
+    {
+        if ($this->config->getCurrentPage() - $this->left_offset < 1) {
+            $offset = (int) abs($this->config->getCurrentPage() - 1 - $this->left_offset);
+            $this->left_offset = $this->left_offset - $offset;
+            $this->right_offset = $this->right_offset + $offset;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Adjustment, if the offset is too large right.
+     *
+     * @return NavigateRange
+     */
+    protected function adjustmentLargeRightOffset()
+    {
+        if ($this->config->getCurrentPage() + $this->right_offset > $this->config->getTotalPages()) {
+            $offset = (int) abs(
+                $this->config->getTotalPages() -
+                $this->config->getCurrentPage() -
+                $this->right_offset
+            );
+            $this->left_offset = $this->left_offset + $offset;
+            $this->right_offset = $this->right_offset - $offset;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Left offset should point not lower of the first page.
+     *
+     * @return NavigateRange
+     */
+    protected function adjustmentLowerLeftOffset()
+    {
+        if ($this->left_offset >= $this->config->getCurrentPage()) {
+            $this->left_offset = $this->config->getCurrentPage() - 1;
         }
 
         return $this;
