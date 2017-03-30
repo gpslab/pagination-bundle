b1dadf2a007154f90c301f4f60e064153d8e77b1
Peter Gribanov <wcode404@gmail.com>
2017-03-30 17:33:10 +0300

    test get container extension from bundle

---
+++ tests/GpsLabPaginationBundleTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Bundle\PaginationBundle\Tests;
+
+use GpsLab\Bundle\PaginationBundle\GpsLabPaginationBundle;
+
+class GpsLabPaginationBundleTest extends TestCase
+{
+    public function testGetContainerExtension()
+    {
+        $bundle = new GpsLabPaginationBundle();
+        $extension = $bundle->getContainerExtension();
+
+        $this->assertInstanceOf(
+            'GpsLab\Bundle\PaginationBundle\DependencyInjection\GpsLabPaginationExtension',
+            $extension
+        );
+    }
+}
