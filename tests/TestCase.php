99b716e9b8304d4610dc1f1248b94487e20cd0d3
Peter Gribanov <wcode404@gmail.com>
2019-10-30 17:43:23 +0300

    not test unit tests with PHPStan

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,49 +8,36 @@
  * @license   http://opensource.org/licenses/MIT
  */
 
-// hook for support PHPUnit 5.7 and newer versions #21
+namespace GpsLab\Bundle\PaginationBundle\Tests;
 
-namespace {
-    use PHPUnit\Framework\TestCase;
-
-    if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
-        class PHPUnit_Framework_TestCase extends TestCase
-        {
-        }
-    }
-}
-
-namespace GpsLab\Bundle\PaginationBundle\Tests
+class TestCase extends \PHPUnit_Framework_TestCase
 {
-    class TestCase extends \PHPUnit_Framework_TestCase
+    /**
+     * @param string $class_name
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getMockNoConstructor($class_name)
     {
-        /**
-         * @param string $class_name
-         *
-         * @return \PHPUnit_Framework_MockObject_MockObject
-         */
-        protected function getMockNoConstructor($class_name)
-        {
-            return $this
-                ->getMockBuilder($class_name)
-                ->disableOriginalConstructor()
-                ->disableOriginalClone()
-                ->getMock();
-        }
+        return $this
+            ->getMockBuilder($class_name)
+            ->disableOriginalConstructor()
+            ->disableOriginalClone()
+            ->getMock();
+    }
 
-        /**
-         * @param string $class_name
-         * @param array  $methods
-         *
-         * @return \PHPUnit_Framework_MockObject_MockObject
-         */
-        protected function getMockAbstract($class_name, array $methods)
-        {
-            return $this
-                ->getMockBuilder($class_name)
-                ->disableOriginalConstructor()
-                ->setMethods($methods)
-                ->getMockForAbstractClass();
-        }
+    /**
+     * @param string $class_name
+     * @param array  $methods
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getMockAbstract($class_name, array $methods)
+    {
+        return $this
+            ->getMockBuilder($class_name)
+            ->disableOriginalConstructor()
+            ->setMethods($methods)
+            ->getMockForAbstractClass();
     }
 }
